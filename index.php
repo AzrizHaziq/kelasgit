d503f8f687ab014b45e8bc58c9a2d7c8b4d31444
Haziq <haziq@nazrol.tech>
2016-09-29 08:36:34 +0800

    Newbranch commit 2

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 	echo 'newbranch line: 1';
 	echo 'newbranch line: 2';
+	echo 'newbranch line: 3';
+	echo 'newbranch line: 4';
 	echo "Hello git asdasdasd";
 	echo "asdasdasdasdsdasdasd";
 	ehcho "asdasdsadasds Hello asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd";
--- user.php
+++ user.php
@@ -1,5 +1,7 @@
 newbranch line: 1
 newbranch line: 2
+newbranch line: 3
+newbranch line: 4
 TRY TRY TRY
 
 branch
