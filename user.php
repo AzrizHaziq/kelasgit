962c56946b339678de937b9aa7b9c3592000b20e
Haziq <haziq@nazrol.tech>
2016-09-29 09:07:52 +0800

    Newbranch 1

--- user.php
+++ user.php
@@ -1,11 +1,18 @@
 newbranch line: 1
 newbranch line: 2
+<<<<<<< 25c212a5636df88e8c2b9c7c4be2030c4f8ec673
 Master line: 1a
 Master line: 2a
 master line: 1
 master line: 2
 master line: 3
 master line: 4
+=======
+newbranch line: 3
+newbranch line: 4
+newbranch line: 5
+newbranch line: 6
+>>>>>>> Newbranch 1
 TRY TRY TRY
 
 branch
