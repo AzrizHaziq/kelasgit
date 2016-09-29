39207e12342152b37de61df2d63af469abc7069d
Haziq <haziq@nazrol.tech>
2016-09-29 08:27:07 +0800

    Master commit 1

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-
+	echo 'master line: 1';
+	echo 'master line: 2';
 	echo "Hello git asdasdasd";
 	echo "asdasdasdasdsdasdasd";
 	ehcho "asdasdsadasds Hello asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd";
-?>	
+?>
--- user.php
+++ user.php
@@ -1,4 +1,5 @@
-dasdasdasd
+master line: 1
+master line: 2
 TRY TRY TRY
 
 branch
