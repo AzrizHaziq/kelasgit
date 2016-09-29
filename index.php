a53d34185e5fecc20889bf608e1d640f0235a2c8
Haziq <haziq@nazrol.tech>
2016-09-29 09:09:20 +0800

    Master 1

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 	echo 'newbranch line: 2';
 	echo 'newbranch line: 3';
 	echo 'newbranch line: 4';
+	echo 'master line: 1';
+	echo 'master line: 2';
 	echo "Hello git asdasdasd";
 	echo "asdasdasdasdsdasdasd";
 	ehcho "asdasdsadasds Hello asddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd";
--- user.php
+++ user.php
@@ -2,6 +2,8 @@ newbranch line: 1
 newbranch line: 2
 newbranch line: 3
 newbranch line: 4
+master line: 1
+master line: 2
 TRY TRY TRY
 
 branch
